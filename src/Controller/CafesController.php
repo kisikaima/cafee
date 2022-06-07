c93913e593be0ab12ca453c22b2f503e3aeac6a0
岸魁馬 <>
2022-06-08 00:08:16 +0900

    docker

--- src/Controller/CafesController.php
+++ src/Controller/CafesController.php
@@ -10,5 +10,16 @@ class CafesController extends AppController
     }
     public function pre(){
         $this->layout = "";
+        if (isset($this->request->data['login'])) {
+
+            $user = $this->Auth->identify();  //←①
+
+            if ($user) {
+                $this->Auth->setUser($user);  //←②
+                return $this->redirect($this->Auth->redirectUrl());  //←③
+            } else {
+                //エラー時の処理
+            }
+        } 
     }
 }
